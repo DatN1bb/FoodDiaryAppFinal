30c4db7ffa8a2ff1ca974659f6f07d7f7649ef8d
Nick Dew <franjogames15@gmail.com>
2025-10-07 10:58:33 +0200

    Add /analyze route to FastAPI

--- api/index.py
+++ api/index.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from fastapi import Form
 
 app = FastAPI()
 
@@ -13,3 +14,7 @@ templates = Jinja2Templates(directory="app/templates")
 async def home(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
+@app.post("/analyze")
+async def analyze(food: str = Form(...)):
+    result = analyze_food(food)  # your function
+    return {"analysis": result}
